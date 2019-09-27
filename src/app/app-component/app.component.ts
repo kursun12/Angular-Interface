3e39986ab865d12c57750eecf96c90c762a9e2f5
SaliihK-PC\SaliihK <salih_kara@hotmail.nl>
2019-09-27 14:54:58 +0200

    imported font awesome icon

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTimes);
+
 
 @Component({
   selector: 'app-root',
