80347b37243a96d26578c8b4b6733e04d84f8843
SaliihK-PC\SaliihK <salih_kara@hotmail.nl>
2019-10-01 11:36:02 +0200

    update records

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimes, faPlus} from '@fortawesome/free-solid-svg-icons';
-import { without } from 'lodash';
+import { without, findIndex } from 'lodash';
 
 library.add(faTimes, faPlus);
 
@@ -11,21 +11,37 @@ library.add(faTimes, faPlus);
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit {  //variabels worden hier gedeclareerd
   title = 'Wisdom Pet Medicine';
   theList: object[];
   modiefiedList: object[];
   orderBy: string;
   orderType: string;
+  lastIndex: number;
+
+  //methdoes hieronder
 
   deleteApt(theApt: object) {
     this.theList = without(this.theList, theApt);
     this.modiefiedList = without(this.modiefiedList, theApt);
   }
 
-  addApt(theApt: object) {
+  addApt(theApt: any) {
+    theApt.aptId = this.lastIndex;
     this.theList.unshift(theApt);
     this.modiefiedList.unshift(theApt);
+    this.lastIndex++;
+  }
+
+  updateApt(aptInfo) {
+    let aptIndex: number;
+    let modifiedIndex: number;
+
+    aptIndex = findIndex(this.theList, { aptId: aptInfo.theApt.aptId})
+    modifiedIndex = findIndex(this.modiefiedList, { aptId: aptInfo.theApt.aptId})
+
+    this.theList[aptIndex][aptInfo.labelname] = aptInfo.newValue;
+    this.modiefiedList[aptIndex][aptInfo.labelname] = aptInfo.newValue;
   }
 
   searchApt(theQuery: string) {
@@ -62,6 +78,7 @@ export class AppComponent implements OnInit {
   }
 
   orderApt(orderObj) {
+    
     this.orderBy = orderObj.orderBy;
     this.orderType = orderObj.orderType;
 
@@ -75,8 +92,13 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.lastIndex = 0;
     this.http.get<Object[]>('../assets/data.json').subscribe(data => {
-      this.theList = data;
+      this.theList = data.map((item: any) => {
+        item.aptId = this.lastIndex++;
+        return item;
+      });
+
       this.modiefiedList = data;
 
       this.sortItems();
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -8,9 +8,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ListComponent {
   @Input() aptList;
   @Output() deleteEvent = new EventEmitter();
+  @Output() updateEVT = new EventEmitter();
 
   handleDelete(theApt: object) {  // method/event
     console.log(theApt);
     this.deleteEvent.emit(theApt);
+  }
+
+  handleUpdate(theApt: object, labelName: string, newValue: string) {
+    this.updateEVT.emit({
+      theApt: theApt,
+      labelName: labelName,
+      newValue: newValue
+    })
   }
 }
